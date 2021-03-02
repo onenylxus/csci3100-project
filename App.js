297e87db1cc6f3b5e3d368d346ef3a2251c35e5f
OneNylxus <onenylxus@gmail.com>
2021-03-02 17:51:55 +0800

    Change license from MIT to Apache 2.0

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 // Import
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 // Stylesheet
 const styles = StyleSheet.create({
@@ -15,9 +15,15 @@ const styles = StyleSheet.create({
 
 // Export application
 export default function App() {
+  const [count, setCount] = React.useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>CU There</Text>
+      <Button
+        title="Click here to increment"
+        onPress={() => setCount(count + 1)}
+      />
+      <Text>{count}</Text>
       <StatusBar style="auto" />
     </View>
   );
