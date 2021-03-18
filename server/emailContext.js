7fea8afd2edb9e86e2ebafb6c17bbba9075c8f09
OneNylxus <onenylxus@gmail.com>
2021-03-18 21:21:16 +0800

    Minor eslint fix

--- server/emailContext.js
+++ server/emailContext.js
@@ -3,7 +3,8 @@ const text = (code) =>
   `Hi there,\n\n We are happy that you signed up for CUThere! To continue the verification process, please copy the following key and paste it in the verification page.\n\n Your key: ${code}\n\nWelcome to CUThere!\nThe CUThere Team`;
 
 // Email HTML
-const html = (code) => `Hi there,<br /><br /> We are happy that you signed up for CUThere! To continue the verification process, please copy the following key and paste it in the verification page.<br /><br /> Your key: ${code}<br /><br />Welcome to CUThere!<br />The CUThere Team`;
+const html = (code) =>
+  `Hi there,<br /><br /> We are happy that you signed up for CUThere! To continue the verification process, please copy the following key and paste it in the verification page.<br /><br /> Your key: ${code}<br /><br />Welcome to CUThere!<br />The CUThere Team`;
 
 // Export
 module.exports = { text, html };
