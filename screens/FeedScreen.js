a9a8a56431c6726b4976904a6b56b1e247d5c737
Pan LIU <liuhoipan@gmail.com>
2021-03-18 14:17:11 +0800

    HomeScreen ScrollView

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,13 +1,22 @@
 // Import
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import Style from '../assets/style';
 
 // Export home screen
 export default function HomeScreen() {
   return (
     <View style={Style.container}>
-      <Text>Homepage</Text>
+      <ScrollView>
+        <Text>Homepage</Text>
+        <Text style={{ fontSize: 96 }}>Testing</Text>
+        <Text style={{ fontSize: 96 }}>Scroll</Text>
+        <Text style={{ fontSize: 96 }}>Testing</Text>
+        <Text style={{ fontSize: 96 }}>Testing</Text>
+        <Text style={{ fontSize: 96 }}>Testing</Text>
+        <Text style={{ fontSize: 96 }}>Post 1</Text>
+        <Text style={{ fontSize: 96 }}>Post 2</Text>
+      </ScrollView>
     </View>
   );
 }
