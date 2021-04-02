564dca8ee0bbb6c1488f0d7315e5372409e24a8d
winkydes <keithlam0110@gmail.com>
2021-04-02 22:23:57 +0800

    Remake structure of app
    Co-authored-by: Pan Liu <panliu0310@users.noreply.github.com>
    Co-authored-by: Nicholas Ng <onenylxus@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,36 +1,99 @@
 // Import
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import ForgotPasswordStack from './screens/navigators/ForgotPasswordStack';
-import Header from './components/Header';
-import HomeTab from './screens/navigators/HomeTab';
-import LoginScreen from './screens/LoginScreen';
-import RegisterStack from './screens/navigators/RegisterStack';
-import VerificationScreen from './screens/VerificationScreen';
-import EditProfileScreen from './screens/EditProfileScreen';
+import {
+  faComment,
+  faHome,
+  faPlus,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import AddInfoScreen from './screens/AddInfoScreen';
+import ChatroomStack from './screens/stacks/ChatroomStack';
+import CreatePostStack from './screens/stacks/CreatePostStack';
+import FeedStack from './screens/stacks/FeedStack';
+import ForgotPasswordStack from './screens/stacks/ForgotPasswordStack';
+import Header from './components/Header';
+import LoginScreen from './screens/LoginScreen';
+import ProfileStack from './screens/stacks/ProfileStack';
+import RegisterStack from './screens/stacks/RegisterStack';
+import VerificationScreen from './screens/VerificationScreen';
+// import EditProfileScreen from './screens/EditProfileScreen';
 
 // Stack
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 // Export application
 export default function App() {
+  // Client state
+  const user = '';
+
+  // No header option
   function NoHeader() {
     return { headerShown: false };
   }
-  // prettier-ignore
-  return (
+
+  return user !== '' ? (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => {
+            switch (route.name) {
+              case 'Chatroom':
+                return <FontAwesomeIcon icon={faComment} color={color} />;
+              case 'CreatePost':
+                return <FontAwesomeIcon icon={faPlus} color={color} />;
+              case 'Feed':
+                return <FontAwesomeIcon icon={faHome} color={color} />;
+              case 'Profile':
+                return <FontAwesomeIcon icon={faUser} color={color} />;
+              default:
+                return new Error();
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+          scrollEnabled: false,
+        }}
+      >
+        <Tab.Screen name="Feed" component={FeedStack} />
+        <Tab.Screen name="CreatePost" component={CreatePostStack} />
+        <Tab.Screen name="Chatroom" component={ChatroomStack} />
+        <Tab.Screen name="Profile" component={ProfileStack} />
+      </Tab.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  ) : (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="ForgotPassword" component={ForgotPasswordStack} options={NoHeader} />
-        <Stack.Screen name="Tabs" component={HomeTab} options={Header} />
-        <Stack.Screen name="Verification" component={VerificationScreen} options={NoHeader} />
-        <Stack.Screen name="Login" component={LoginScreen} options={Header} />
-        <Stack.Screen name="Register" component={RegisterStack} options={Header} />
-        <Stack.Screen name="EditProfile" component={EditProfileScreen} options={Header} />
-        <Stack.Screen name="AddInfo" component={AddInfoScreen} options={NoHeader} />
+        <Stack.Screen
+          name="AddInfo"
+          component={AddInfoScreen}
+          options={NoHeader}
+        />
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPasswordStack}
+          option={NoHeader}
+        />
+        <Stack.Screen name="Login" component={LoginScreen} option={Header} />
+        <Stack.Screen
+          name="Register"
+          component={RegisterStack}
+          options={Header}
+        />
+        <Stack.Screen
+          name="Verification"
+          component={VerificationScreen}
+          option={NoHeader}
+        />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
