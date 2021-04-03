6fa7cea2e6aca09cea5554dd41483569b5909cf2
winkydes <keithlam0110@gmail.com>
2021-04-03 18:59:48 +0800

    Rewrite program codes for majorPicker

--- components/AddInfoForm.js
+++ components/AddInfoForm.js
@@ -96,12 +96,119 @@ export default function AddInfoForm() {
 
       {/* Major */}
       <Text style={Style.sectionText}>Major:</Text>
-      <View style={Style.SectionStyle}>
-        <TextInput
-          style={Style.textInput}
-          placeholder="Major"
-          onChangeText={(text) => setMajor(text)}
-        />
+      <View>
+        <Picker
+          selectedValue={major}
+          onValueChange={(itemValue) => setMajor(itemValue)}
+        >
+          <Picker.Item label="Select an option" value="" />
+          <Picker.Item label="Anthropology" value="ANTHN" />
+          <Picker.Item label="Architectural Studies" value="ARCHN" />
+          <Picker.Item
+            label="Artificial Intelligence: Systems and Technologies"
+            value="AISTN"
+          />
+          <Picker.Item label="Bimodal Bilingual Studies" value="BMBLN" />
+          <Picker.Item label="Biochemistry" value="BCHEN" />
+          <Picker.Item label="Biology" value="BIOLN" />
+          <Picker.Item label="Biomedical Engineering" value="BMEGN" />
+          <Picker.Item label="Biomedical Sciences" value="BMSCN" />
+          <Picker.Item label="Cell and Molecular Biology" value="CMBIN" />
+          <Picker.Item label="Chemistry" value="CHEMN" />
+          <Picker.Item label="Chinese Medicine" value="BCMEN" />
+          <Picker.Item label="Chinese Language and Literature" value="CHLLN" />
+          <Picker.Item label="Chinese Studies" value="CHESB" />
+          <Picker.Item label="Community Health Practice" value="CHPRN" />
+          <Picker.Item label="Computer Engineering" value="CENGN" />
+          <Picker.Item label="Computer Science" value="CSCIN" />
+          <Picker.Item label="Cultural Management" value="CUMTN" />
+          <Picker.Item label="Cultural Studies" value="Cultural Studies" />
+          <Picker.Item label="Data Science and Policy Studies" value="DSPSN" />
+          <Picker.Item label="Early Childhood Education" value="CHEDN" />
+          <Picker.Item label="Earth System Science" value="ESSCN" />
+          <Picker.Item label="Economics" value="ECONN" />
+          <Picker.Item label="Electronic Engineering" value="ELEGN" />
+          <Picker.Item
+            label="Energy and Environmental Engineering"
+            value="EEENN"
+          />
+          <Picker.Item label="English" value="ENGEN" />
+          <Picker.Item label="Environmental Science" value="ENSCN" />
+          <Picker.Item
+            label="Exercise Science and Health Education"
+            value="ESHEN"
+          />
+          <Picker.Item label="Financial Technology" value="FTECN" />
+          <Picker.Item label="Fine Arts" value="FAASN" />
+          <Picker.Item label="Food and Nutritional Sciences" value="FNSCN" />
+          <Picker.Item label="Gender Studies" value="GDRSN" />
+          <Picker.Item
+            label="Geography and Resource Management"
+            value="GRMDN"
+          />
+          <Picker.Item label="Gerontology" value="BSCGN" />
+          <Picker.Item label="Global Business Studies" value="IBBAB" />
+          <Picker.Item label="Global Communication" value="GCOMN" />
+          <Picker.Item label="Global Studies" value="GLBSN" />
+          <Picker.Item
+            label="Government and Public Administration"
+            value="GPADN"
+          />
+          <Picker.Item label="History" value="History" />
+          <Picker.Item label="Hospitality and Real Estate" value="HTMGB" />
+          <Picker.Item label="Information Engineering" value="IERGN" />
+          <Picker.Item
+            label="Insurance, Financial and Actuarial Analysis"
+            value="IFAAB"
+          />
+          <Picker.Item label="Integrated BBA" value="IBBAB" />
+          <Picker.Item label="Japanese Studies" value="JASPN" />
+          <Picker.Item label="Journalism and Communication" value="COMMN" />
+          <Picker.Item label="Law" value="LAWSN" />
+          <Picker.Item label="Liberal Studies" value="LSEDN" />
+          <Picker.Item label="Linguistics" value="LINGN" />
+          <Picker.Item label="Mathematics" value="MATHN" />
+          <Picker.Item
+            label="Mathematics and Information Engineering"
+            value="MIEGN"
+          />
+          <Picker.Item
+            label="Mathematics and Mathematics Education"
+            value="BMEDN"
+          />
+          <Picker.Item
+            label="Mechanical and Automation Engineering"
+            value="MAEGN"
+          />
+          <Picker.Item label="Medicine Programme (M.B., Ch.B.)" value="MEDUN" />
+          <Picker.Item label="Molecular Biotechnology" value="MBTEN" />
+          <Picker.Item label="Music" value="MUSCN" />
+          <Picker.Item label="Natural Sciences" value="NSCIN" />
+          <Picker.Item label="Nursing" value="NURSN" />
+          <Picker.Item label="Pharmacy" value="PHARN" />
+          <Picker.Item label="Philosophy" value="PHILN" />
+          <Picker.Item
+            label="Physical Education, Exercise Science and Health"
+            value="PESHN"
+          />
+          <Picker.Item label="Physics" value="PHYSN" />
+          <Picker.Item label="Professional Accountancy" value="PACCN" />
+          <Picker.Item label="Psychology" value="PSYCN" />
+          <Picker.Item label="Public Health" value="PHPCN" />
+          <Picker.Item label="Quantitative Finance" value="QFINN" />
+          <Picker.Item label="Religious Studies" value="RELSN" />
+          <Picker.Item label="Risk Management Science" value="RMSCN" />
+          <Picker.Item label="Social Work" value="SOWKN" />
+          <Picker.Item label="Sociology" value="SOCIN" />
+          <Picker.Item label="Statistics" value="STATN" />
+          <Picker.Item
+            label="Systems Engineering and Engineering Management"
+            value="SEEMN"
+          />
+          <Picker.Item label="Theology" value="THEON" />
+          <Picker.Item label="Translation" value="TRANN" />
+          <Picker.Item label="Urban Studies" value="URSPN" />
+        </Picker>
       </View>
 
       {/* College */}
@@ -110,11 +217,16 @@ export default function AddInfoForm() {
         selectedValue={college}
         onValueChange={(itemValue) => setCollege(itemValue)}
       >
-        <Picker.Item label="Select a college" value="" />
-        <Picker.Item label="New Asia College" value="NA" />
+        <Picker.Item label="Select an option" value="" />
         <Picker.Item label="Chung Chi College" value="CC" />
+        <Picker.Item label="New Asia College" value="NA" />
         <Picker.Item label="United College" value="UC" />
         <Picker.Item label="Shaw College" value="SC" />
+        <Picker.Item label="Morningside College" value="MC" />
+        <Picker.Item label="S.H. Ho College" value="SH" />
+        <Picker.Item label="CW Chu College" value="WC" />
+        <Picker.Item label="Wu Yee Sun College" value="YS" />
+        <Picker.Item label="Lee Wo Sing College" value="WS" />
       </Picker>
 
       <Button title="Submit!" onPress={confirmAddInfo} />
--- components/MajorPicker.js
+++ components/MajorPicker.js
@@ -57,195 +57,122 @@ export default function MajorPicker() {
             selectedValue={major}
             onValueChange={(itemValue) => setMajor(itemValue)}
           >
-            <Picker.Item label="Select an option" value="Select an option" />
-            <Picker.Item label="Anthropology" value="Anthropology" />
-            <Picker.Item
-              label="Architectural Studies"
-              value="Architectural Studies"
-            />
+            <Picker.Item label="Select an option" value="" />
+            <Picker.Item label="Anthropology" value="ANTHN" />
+            <Picker.Item label="Architectural Studies" value="ARCHN" />
             <Picker.Item
               label="Artificial Intelligence: Systems and Technologies"
-              value="Artificial Intelligence: Systems and Technologies"
+              value="AISTN"
             />
-            <Picker.Item
-              label="Bimodal Bilingual Studies"
-              value="Bimodal Bilingual Studies"
-            />
-            <Picker.Item label="Biochemistry" value="Biochemistry" />
-            <Picker.Item label="Biology" value="Biology" />
-            <Picker.Item
-              label="Biomedical Engineering"
-              value="Biomedical Engineering"
-            />
-            <Picker.Item
-              label="Biomedical Sciences"
-              value="Biomedical Sciences"
-            />
-            <Picker.Item
-              label="Cell and Molecular Biology"
-              value="Cell and Molecular Biology"
-            />
-            <Picker.Item label="Chemistry" value="Chemistry" />
-            <Picker.Item label="Chinese Medicine" value="Chinese Medicine" />
+            <Picker.Item label="Bimodal Bilingual Studies" value="BMBLN" />
+            <Picker.Item label="Biochemistry" value="BCHEN" />
+            <Picker.Item label="Biology" value="BIOLN" />
+            <Picker.Item label="Biomedical Engineering" value="BMEGN" />
+            <Picker.Item label="Biomedical Sciences" value="BMSCN" />
+            <Picker.Item label="Cell and Molecular Biology" value="CMBIN" />
+            <Picker.Item label="Chemistry" value="CHEMN" />
+            <Picker.Item label="Chinese Medicine" value="BCMEN" />
             <Picker.Item
               label="Chinese Language and Literature"
-              value="Chinese Language and Literature"
+              value="CHLLN"
             />
-            <Picker.Item label="Chinese Studies" value="Chinese Studies" />
-            <Picker.Item
-              label="Community Health Practice"
-              value="Community Health Practice"
-            />
-            <Picker.Item
-              label="Computer Engineering"
-              value="Computer Engineering"
-            />
-            <Picker.Item label="Computer Science" value="Computer Science" />
-            <Picker.Item
-              label="Cultural Management"
-              value="Cultural Management"
-            />
+            <Picker.Item label="Chinese Studies" value="CHESB" />
+            <Picker.Item label="Community Health Practice" value="CHPRN" />
+            <Picker.Item label="Computer Engineering" value="CENGN" />
+            <Picker.Item label="Computer Science" value="CSCIN" />
+            <Picker.Item label="Cultural Management" value="CUMTN" />
             <Picker.Item label="Cultural Studies" value="Cultural Studies" />
             <Picker.Item
               label="Data Science and Policy Studies"
-              value="Data Science and Policy Studies"
+              value="DSPSN"
             />
-            <Picker.Item
-              label="Early Childhood Education"
-              value="Early Childhood Education"
-            />
-            <Picker.Item
-              label="Earth System Science"
-              value="Earth System Science"
-            />
-            <Picker.Item label="Economics" value="Economics" />
-            <Picker.Item
-              label="Electronic Engineering"
-              value="Electronic Engineering"
-            />
+            <Picker.Item label="Early Childhood Education" value="CHEDN" />
+            <Picker.Item label="Earth System Science" value="ESSCN" />
+            <Picker.Item label="Economics" value="ECONN" />
+            <Picker.Item label="Electronic Engineering" value="ELEGN" />
             <Picker.Item
               label="Energy and Environmental Engineering"
-              value="Energy and Environmental Engineering"
+              value="EEENN"
             />
-            <Picker.Item label="English" value="English" />
-            <Picker.Item
-              label="Environmental Science"
-              value="Environmental Science"
-            />
+            <Picker.Item label="English" value="ENGEN" />
+            <Picker.Item label="Environmental Science" value="ENSCN" />
             <Picker.Item
               label="Exercise Science and Health Education"
-              value="Exercise Science and Health Education"
+              value="ESHEN"
             />
-            <Picker.Item
-              label="Financial Technology"
-              value="Financial Technology"
-            />
-            <Picker.Item label="Fine Arts" value="Fine Arts" />
-            <Picker.Item
-              label="Food and Nutritional Sciences"
-              value="Food and Nutritional Sciences"
-            />
-            <Picker.Item label="Gender Studies" value="Gender Studies" />
+            <Picker.Item label="Financial Technology" value="FTECN" />
+            <Picker.Item label="Fine Arts" value="FAASN" />
+            <Picker.Item label="Food and Nutritional Sciences" value="FNSCN" />
+            <Picker.Item label="Gender Studies" value="GDRSN" />
             <Picker.Item
               label="Geography and Resource Management"
-              value="Geography and Resource Management"
+              value="GRMDN"
             />
-            <Picker.Item label="Gerontology" value="Gerontology" />
-            <Picker.Item
-              label="Global Business Studies"
-              value="Global Business Studies"
-            />
-            <Picker.Item
-              label="Global Communication"
-              value="Global Communication"
-            />
-            <Picker.Item label="Global Studies" value="Global Studies" />
+            <Picker.Item label="Gerontology" value="BSCGN" />
+            <Picker.Item label="Global Business Studies" value="IBBAB" />
+            <Picker.Item label="Global Communication" value="GCOMN" />
+            <Picker.Item label="Global Studies" value="GLBSN" />
             <Picker.Item
               label="Government and Public Administration"
-              value="Government and Public Administration"
+              value="GPADN"
             />
             <Picker.Item label="History" value="History" />
-            <Picker.Item
-              label="Hospitality and Real Estate"
-              value="Hospitality and Real Estate"
-            />
-            <Picker.Item
-              label="Information Engineering"
-              value="Information Engineering"
-            />
+            <Picker.Item label="Hospitality and Real Estate" value="HTMGB" />
+            <Picker.Item label="Information Engineering" value="IERGN" />
             <Picker.Item
               label="Insurance, Financial and Actuarial Analysis"
-              value="Insurance, Financial and Actuarial Analysis"
+              value="IFAAB"
             />
-            <Picker.Item label="Integrated BBA" value="Integrated BBA" />
-            <Picker.Item label="Japanese Studies" value="Japanese Studies" />
-            <Picker.Item
-              label="Journalism and Communication"
-              value="Journalism and Communication"
-            />
-            <Picker.Item label="Law" value="Law" />
-            <Picker.Item label="Liberal Studies" value="Liberal Studies" />
-            <Picker.Item label="Linguistics" value="Linguistics" />
-            <Picker.Item label="Mathematics" value="Mathematics" />
+            <Picker.Item label="Integrated BBA" value="IBBAB" />
+            <Picker.Item label="Japanese Studies" value="JASPN" />
+            <Picker.Item label="Journalism and Communication" value="COMMN" />
+            <Picker.Item label="Law" value="LAWSN" />
+            <Picker.Item label="Liberal Studies" value="LSEDN" />
+            <Picker.Item label="Linguistics" value="LINGN" />
+            <Picker.Item label="Mathematics" value="MATHN" />
             <Picker.Item
               label="Mathematics and Information Engineering"
-              value="Mathematics and Information Engineering"
+              value="MIEGN"
             />
             <Picker.Item
               label="Mathematics and Mathematics Education"
-              value="Mathematics and Mathematics Education"
-            />
-            <Picker.Item
-              label="M.B., Ch.B. − Global Physician-Leadership Stream"
-              value="M.B., Ch.B. − Global Physician-Leadership Stream"
+              value="BMEDN"
             />
             <Picker.Item
               label="Mechanical and Automation Engineering"
-              value="Mechanical and Automation Engineering"
+              value="MAEGN"
             />
             <Picker.Item
               label="Medicine Programme (M.B., Ch.B.)"
-              value="Medicine Programme (M.B., Ch.B.)"
+              value="MEDUN"
             />
-            <Picker.Item
-              label="Molecular Biotechnology"
-              value="Molecular Biotechnology"
-            />
-            <Picker.Item label="Music" value="Music" />
-            <Picker.Item label="Natural Sciences" value="Natural Sciences" />
-            <Picker.Item label="Nursing" value="Nursing" />
-            <Picker.Item label="Pharmacy" value="Pharmacy" />
-            <Picker.Item label="Philosophy" value="Philosophy" />
+            <Picker.Item label="Molecular Biotechnology" value="MBTEN" />
+            <Picker.Item label="Music" value="MUSCN" />
+            <Picker.Item label="Natural Sciences" value="NSCIN" />
+            <Picker.Item label="Nursing" value="NURSN" />
+            <Picker.Item label="Pharmacy" value="PHARN" />
+            <Picker.Item label="Philosophy" value="PHILN" />
             <Picker.Item
               label="Physical Education, Exercise Science and Health"
-              value="Physical Education, Exercise Science and Health"
+              value="PESHN"
             />
-            <Picker.Item label="Physics" value="Physics" />
-            <Picker.Item
-              label="Professional Accountancy"
-              value="Professional Accountancy"
-            />
-            <Picker.Item label="Psychology" value="Psychology" />
-            <Picker.Item label="Public Health" value="Public Health" />
-            <Picker.Item
-              label="Quantitative Finance"
-              value="Quantitative Finance"
-            />
-            <Picker.Item label="Religious Studies" value="Religious Studies" />
-            <Picker.Item
-              label="Risk Management Science"
-              value="Risk Management Science"
-            />
-            <Picker.Item label="Social Work" value="Social Work" />
-            <Picker.Item label="Sociology" value="Sociology" />
-            <Picker.Item label="Statistics" value="Statistics" />
+            <Picker.Item label="Physics" value="PHYSN" />
+            <Picker.Item label="Professional Accountancy" value="PACCN" />
+            <Picker.Item label="Psychology" value="PSYCN" />
+            <Picker.Item label="Public Health" value="PHPCN" />
+            <Picker.Item label="Quantitative Finance" value="QFINN" />
+            <Picker.Item label="Religious Studies" value="RELSN" />
+            <Picker.Item label="Risk Management Science" value="RMSCN" />
+            <Picker.Item label="Social Work" value="SOWKN" />
+            <Picker.Item label="Sociology" value="SOCIN" />
+            <Picker.Item label="Statistics" value="STATN" />
             <Picker.Item
               label="Systems Engineering and Engineering Management"
-              value="Systems Engineering and Engineering Management"
+              value="SEEMN"
             />
-            <Picker.Item label="Theology" value="Theology" />
-            <Picker.Item label="Translation" value="Translation" />
-            <Picker.Item label="Urban Studies" value="Urban Studies" />
+            <Picker.Item label="Theology" value="THEON" />
+            <Picker.Item label="Translation" value="TRANN" />
+            <Picker.Item label="Urban Studies" value="URSPN" />
           </Picker>
         </View>
       </View>
