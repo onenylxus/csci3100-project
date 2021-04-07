5ab96272e91ed872f628203ad8f88b668a4408a6
OneNylxus <onenylxus@gmail.com>
2021-04-07 19:14:33 +0800

    Jest test setup

---
+++ __tests__/App.test.js
@@ -0,0 +1,19 @@
+// Import
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+// Import target component
+import App from '../App';
+
+// Mock FontAwesome icons
+jest.mock('@fortawesome/react-native-fontawesome', () => ({
+  FontAwesomeIcon: '',
+}));
+
+// Run jest tests
+describe('Main application', () => {
+  it('compiles successfully', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
