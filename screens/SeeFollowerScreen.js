8688f5715900cf858f109fc4d64c0c168b8e2e11
winkydes <75211259+winkydes@users.noreply.github.com>
2021-04-14 05:28:00 +0800

    Show follow (#35)

---
+++ screens/SeeFollowerScreen.js
@@ -0,0 +1,55 @@
+// Import
+import React from 'react';
+import { View, ScrollView } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import FollowerContainer from '../components/FollowerContainer';
+
+// Export seeFollower screen
+export default function SeeFollowerScreen() {
+  const route = useRoute();
+  const { username } = route.params;
+  const [list, setList] = React.useState([]);
+
+  const status = React.useRef(0);
+
+  function fetchFollow() {
+    (async () => {
+      await fetch(`https://cu-there-server.herokuapp.com/fetchFollow`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          other: username,
+        }),
+      })
+        .then((res) => {
+          status.current = res.status;
+          return res;
+        })
+        .then((res) => res.json())
+        .then((res) => {
+          if (status.current === 200) {
+            setList(res.follower);
+          } else if (status.current === 422) {
+            console.log(res.error);
+          }
+        })
+        .catch((err) => console.log(err));
+    })();
+  }
+
+  function generate() {
+    return list.map((data) => <FollowerContainer username={data} key={data} />);
+  }
+
+  React.useEffect(fetchFollow);
+
+  return (
+    <View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View>{generate()}</View>
+      </ScrollView>
+    </View>
+  );
+}
