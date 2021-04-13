193f9d31d865da83c1ec1b0b7f06cb1b65ce7639
winkydes <keithlam0110@gmail.com>
2021-04-14 02:53:29 +0800

    Fixed web version style

---
+++ components/CreateCommentForm.web.js
@@ -0,0 +1,110 @@
+// Import
+import React from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import AwesomeAlert from 'react-native-awesome-alerts';
+import AuthContext from './AuthContext';
+import Style from '../assets/style';
+
+// Export Comment
+export default function CreateCommentForm({ post }) {
+  const { getUser } = React.useContext(AuthContext);
+
+  const [username, setUsername] = React.useState('');
+  const [comment, setComment] = React.useState('');
+
+  const [alert, setAlert] = React.useState(false);
+
+  const status = React.useRef(0);
+  const postId = React.useRef(post._id);
+
+  // Get username
+  getUser(setUsername);
+
+  function showAlert() {
+    setAlert(true);
+  }
+
+  function hideAlert() {
+    setAlert(false);
+  }
+
+  async function submitData() {
+    await fetch(`https://cu-there-server.herokuapp.com/createComment`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        content: comment,
+        _id: postId.current,
+      }),
+    })
+      .then((res) => {
+        status.current = res.status;
+        return res;
+      })
+      .then((res) => res.json())
+      .then((res) => {
+        if (status.current === 200) {
+          setComment('');
+        } else if (status.current === 422) {
+          switch (res.error) {
+            // Empty content
+            case 'missingContentError':
+              return showAlert();
+
+            default:
+              return new Error();
+          }
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <View style={Style.commentBar}>
+      <FontAwesomeIcon
+        icon={faPen}
+        size={15}
+        style={{ marginHorizontal: 10, marginVertical: 12 }}
+      />
+      <TextInput
+        placeholder="Write Comment"
+        style={{ width: '85%', outline: 'none' }}
+        value={comment}
+        onChangeText={(value) => setComment(value)}
+      />
+      {comment !== '' ? (
+        <TouchableOpacity style={Style.sendMessageButtom} onPress={submitData}>
+          <Text style={{ color: 'blue', marginRight: 5 }}>Send</Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          style={Style.sendMessageButtom}
+          onPress={submitData}
+          disabledw
+        >
+          <Text style={{ color: 'blue', marginRight: 5 }}>Send</Text>
+        </TouchableOpacity>
+      )}
+      <AwesomeAlert
+        show={alert}
+        showProgress={false}
+        title="Comment is blank"
+        message="Please enter something to comment."
+        closeOnHardwareBackPress
+        closeOnTouchOutside
+        showConfirmButton
+        confirmText="OK"
+        confirmButtonColor="#DD6B55"
+        onConfirmPressed={() => {
+          hideAlert();
+        }}
+        contentContainerStyle={100}
+      />
+    </View>
+  );
+}
