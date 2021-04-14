9e62071cf2b37010ff63256c13db438df1b2a811
Pan LIU <liuhoipan@gmail.com>
2021-04-14 09:53:35 +0800

    Comment navigation

--- server/controllers/deleteAccount.js
+++ server/controllers/deleteAccount.js
@@ -18,8 +18,6 @@ module.exports = function deleteAccount(req, res) {
 
   // Fetch client
   const client = Client.findOne({ username });
-  // const comment = Comment.find({ username });
-  // const post = Post.find({ username });
 
   client.then((data) => {
     console.log('data: ' + data);
@@ -45,31 +43,7 @@ module.exports = function deleteAccount(req, res) {
         console.log(err);
       }
     );
-    /*
-    comment.then((data1) => {
-      if (data1) {
-        console.log('data1 before delete: ' + data1);
-        data1.updateMany({ $set: { username: 'Deleted User' } }).exec();
-      }
-      console.log('data1 after delete: ' + data1);
-    });
-    */
 
-    /*
-    post.then((data2) => {
-      if (data2) {
-        console.log('data2: ' + data2);
-        data2[0]
-          .update({
-            $set: { username: 'Deleted User' },
-          })
-          .exec();
-      }
-    });
-    */
-
-    // const post = Post.updateMany({ username }, { username: 'Deleted User' });
-    // Post.where({ username }).update({ $set: { username: 'Deleted' } });
     return res.status(200).send({ msg: 'Deleted post and comment' });
   });
 };
